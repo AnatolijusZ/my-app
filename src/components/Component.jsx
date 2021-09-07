8f26790c54d29f39703446aa662c0f59bd5a8082
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-07 15:36:32 +0300

    props

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,15 +1,15 @@
 //import person from '../Data/person';
 
 //Komponentas
-function Element() {
+function Component(props) {
     return (
 
     <div>
     <h1>Hello, Barsukas!</h1>
     <small>Labas Bebras {5 + 8}</small>
     <br></br>
-    <small>No persons</small>
+    <small>{props.name} {props.surname} per {props.per}</small>
     </div>
     );
 }
-    export default Element;
+    export default Component;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Element from './components/element';
+import App from './components/App';
 
 
 ReactDOM.render(
-  <Element/>,
+  <App per={'griovį'} />,
   document.getElementById('root')
 );
 

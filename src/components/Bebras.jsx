266f96c3f224048c96e788595d001fb3d2f1d355
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-08 19:59:19 +0300

    state

--- src/components/Bebras.jsx
+++ src/components/Bebras.jsx
@@ -1,6 +1,45 @@
-function Bebras(props) {
-    return (
-        <span>{props.bebras}</span>
-    );
+import React from 'react';
+
+class Bebras extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()}; //uzduoda busena
+    }    
+    tick() {
+    //tick = () => {
+        this.setState({ date: new Date()}); //setState keicia "atnaujina" state
+      }
+
+    componentDidMount() { 
+        console.log('jau', this.props.bebras);
+        console.log(this.state.date.toLocaleTimeString());
+
+        //document.querySelector('.kvadratas').
+        //addEventListener('click', this.tick); 
+        
+    
+        this.timerID = setInterval( () => this.tick(), 1000 );
+        //this.timerID = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    render() {
+        return (
+            <span>
+                {this.props.bebras}
+                <hr/>
+                {this.state.date.toLocaleTimeString()}
+            </span>
+        );    
+    }
 }
+//function Bebras(props) {
+//    return (
+//        <span>{props.bebras}</span>
+//    );
+//}
     export default Bebras;

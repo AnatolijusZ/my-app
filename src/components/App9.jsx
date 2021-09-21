d6d1931f030554d7cda0f7da5273bd6072cb2c20
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-21 11:00:57 +0300

    farm with hooks and useeffect

---
+++ src/components/App9.jsx
@@ -0,0 +1,9 @@
+import M3F from './M3f'
+
+function App(props) {
+    return ( <>
+
+<M3F> </M3F>
+    </>);
+}
+    export default App;
---
+++ src/components/SmallAnimalF.jsx
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+
+function SmallAnimal(props)  {
+
+const [editInput, setEditInput] = useState("");
+
+const editInputHandler= (e) => {
+    setEditInput (e.target.value);
+}
+
+  
+    return (
+    <div className="small-circle" style={
+      {backgroundColor: props.color,
+      borderRadius: props.animal == 'cow' ? '50%' : '5px'}
+      }>
+      <span>{props.color}</span>
+      <button className="input-button-small" onClick={() => props.delete(props.id)}>KILL</button>
+      <div className="center">
+        <input type="text" value={editInput} onChange={editInputHandler}></input>
+        <button className="input-button-small" onClick={() => props.edit(props.id, editInput)}>Modify</button>
+      </div>
+    </div>
+    )  
+}
+
+    export default SmallAnimal;

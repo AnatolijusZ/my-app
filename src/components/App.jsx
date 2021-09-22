2b59e4a4bb6082749bb25ae0498490e954a6544f
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-22 12:18:29 +0300

    todo users

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,11 +12,33 @@ import Todo from './Todo';
                 console.log(response.data);
                 setTodos(response.data);
             });
-        },[])
+        },[]);
 
+       //const editStatus = (id, completed) => {
+       //     const todosCopy = todos.slice();
+       //     for(let i=0; i<todosCopy.length; i++) {
+       //         if(todosCopy[i].id === id){
+       //             todosCopy[i].completed = !completed;
+       //             break;
+       //         }
+       //     }
+       //     setTodos(todosCopy)
+        //}
+        const sniuriukasTodui = (id) => {
+            const todosCopy2 = todos.slice();
+            for(let i=0; i<todosCopy2.length; i++) {
+                if(id === todosCopy2[i].id){
+                    todosCopy2[i].completed = !todosCopy2[i].completed;
+                    break;
+                }
+            }
+            setTodos(todosCopy2);
+            
+
+        }
     return (
         <div className="todo-container">
-            {todos.map((todo)=>(<Todo key={todo.id} data={todo}></Todo>))}
+            {todos.map((todo)=>(<Todo key={todo.id} data={todo} sniuriukas={sniuriukasTodui}></Todo>))}
         </div>
     )
         
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
+import User from './User';
 
 function Todo(props) {
+
+    //const [checkbox, change] = useState();
+
+   // const swap =() => {
+   //     this.status = !this.status;
+   // };
+
+    //const change =() => {
+     //   console.log('hi');
+    //};
+
+    //change(swap);
+    
 
     return (
         <div className="todo">
             <div className="id">{props.data.id}</div>
+            <User id={props.data.userId}></User>
             <div className="title">{props.data.title}</div>
-            <div className="status" style={
+            <div className="status" onClick={() => props.sniuriukas(props.data.id)} style={
                 {backgroundColor: props.data.completed === false ? 'red' : 'green'}
                 }></div>
         </div>

713de0375a1222c842e2299a4c8f709f8cf1d6a3
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-22 10:50:30 +0300

    todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import React,  {useEffect, useState} from 'react';
 import axios from 'axios';
-
+import Todo from './Todo';
     function App() {
+
+        const [todos, setTodos]=useState([]);
 
         useEffect (() => {
             console.log("Start");
-            axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            axios.get('https://jsonplaceholder.typicode.com/todos')
             .then(function (response) {
                 console.log(response.data);
+                setTodos(response.data);
             });
         },[])
 
-    return (<>
-        Labas
-        </>)
+    return (
+        <div className="todo-container">
+            {todos.map((todo)=>(<Todo key={todo.id} data={todo}></Todo>))}
+        </div>
+    )
+        
     }
 
 export default App;

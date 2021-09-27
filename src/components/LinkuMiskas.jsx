f37430e2a308ea38b380986c2839bec7732b7449
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-27 11:01:23 +0300

    router

---
+++ src/components/LinkuMiskas.jsx
@@ -0,0 +1,31 @@
+import { Link, Switch, Route } from "react-router-dom";
+function LinkuMiskas() {
+
+    return(
+    <div className="forest"><nav>
+        <Link to={'home'}><h3>Linku Miskas</h3></Link>
+        <Link to={'road'}><h3>Misko Takelis</h3></Link>
+        <Link to={'street'}><h3>Misko Plentas</h3></Link>
+    </nav>
+        <Switch>
+
+            <Route path={'/home'}>
+                <h1>The Home</h1>
+            </Route>
+
+            <Route path={'/road'}>
+                <h1>The Road</h1>
+            </Route>
+
+            <Route path={'/street'}>
+                <h1>The Street</h1>
+            </Route>
+
+            <Route path={'/'}>
+                <h1>The begining</h1>
+            </Route>
+        </Switch>
+    </div>)
+}
+
+export default LinkuMiskas;

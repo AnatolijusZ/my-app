261a30866a379ac9b15c20fde9a7a815d19ab6cb
AnatolijusZ <zubkovas.a@gmail.com>
2021-09-30 12:27:49 +0300

    farm with editor window

--- src/components/SmallAnimalF.jsx
+++ src/components/SmallAnimalF.jsx
@@ -1,26 +1,16 @@
 import React,{useState} from 'react';
 
-function SmallAnimal({farmNumber, farm1, color, animal, destroy, change, edit, id})  {
-
-const [editInput, setEditInput] = useState("");
-
-const editInputHandler= (e) => {
-    setEditInput (e.target.value);
-}
+function SmallAnimal({open, farmNumber, farm1, color, animal, id})  {
 
     if ((farmNumber === 1 && farm1) || (farmNumber === 2 && !farm1) ) {
-    return (
-    <div className="small-circle" style={
-      {backgroundColor: color,
-      borderRadius: animal === 'cow' ? '50%' : '5px'}
-      }>
+      return (
+        <div className="small-circle" style={
+        {backgroundColor: color,
+        borderRadius: animal === 'cow' ? '50%' : '5px'}
+        }>
       <span>{color}</span>
-      <button className="input-button-small" onClick={() => destroy(id)}>KILL</button>
-      <button className="input-button-small" onClick={() => change(id)}>Change Farm</button>
-      <div className="center">
-        <input type="text" value={editInput} onChange={editInputHandler}></input>
-        <button className="input-button-small" onClick={() => edit(id, editInput)}>Modify</button>
-      </div>
+      
+      <button className="input-button-small" onClick={() =>open(id)}>Open Edit</button>
     </div>
     )  
 } 
